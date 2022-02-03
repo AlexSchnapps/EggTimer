ddc7d8d63ea22b89cfd24e66c54f2fb0d2bbc230
Alex Schnapps <6kwalitet@gmail.com>
2022-02-03 23:57:20 +0400

    add fich

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -5,7 +5,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
     
+      let eggTimes = [
+        "Soft": 5,
+        "Medium": 7,
+        "Hard": 12
+      ]
+      
+      @IBAction func hardnessSelection(_ sender: UIButton) {
+          guard let hardness = sender.titleLabel?.text else { return }
+          print(hardness)
+          
+          switch hardness {
+              
+          case "Soft":
+              print(eggTimes["Soft"]!)
+          case "Medium":
+              print(eggTimes["Medium"]!)
+          case "Hard":
+              print(eggTimes["Hard"]!)
+          default:
+              print("nil")
+          }
+          
+      }
+      
+  }
 
-
-}
+Блиииин(
