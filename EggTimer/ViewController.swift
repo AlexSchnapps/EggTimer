cb5e298ef62c41d78b62c2e084f21583d17bcfa9
Alex Schnapps <6kwalitet@gmail.com>
2022-02-04 16:11:46 +0400

    Changes Titel Label

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,38 +6,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-      let eggTimes = [
-        "Soft": 300,
-        "Medium": 420,
-        "Hard": 720
-      ]
+    let eggTimes = [
+        "Soft": 3,
+        "Medium": 5,
+        "Hard": 7
+    ]
     
     var timer = Timer()
     
     var secondsRemaining = 60
     
-      @IBAction func hardnessSelection(_ sender: UIButton) {
-          
-          timer.invalidate()
-          
-          guard let hardness = sender.titleLabel?.text else { return }
-          
-          secondsRemaining = eggTimes[hardness]!
-         
-          timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-                  if self.secondsRemaining > 0 {
-                      print ("\(self.secondsRemaining) seconds")
-                      self.secondsRemaining -= 1
-                  } else {
-                      Timer.invalidate()
-                  }
-              }
-      }
-
-
+    //MARK: - IBActions
+    
+    @IBAction func hardnessSelection(_ sender: UIButton) {
+        
+        timer.invalidate()
+        titleLabel.text = "How do you like your eggs?"
+        guard let hardness = sender.titleLabel?.text else { return }
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsRemaining > 0 {
+                print ("\(self.secondsRemaining) seconds")
+                self.secondsRemaining -= 1
+            } else {
+                Timer.invalidate()
+                self.titleLabel.text = "DONE!"
+            }
+        }
+    }
+    
+    
 }
